952805c3a993885f90065d9e6d38b573e981d3ba
Dayeeet <david@cappell.net>
2024-02-25 13:19:28 +0100

    refactor: cleanup ServerHost.kt definition

--- controller-shared/src/main/kotlin/app/simplecloud/controller/shared/host/ServerHost.kt
+++ controller-shared/src/main/kotlin/app/simplecloud/controller/shared/host/ServerHost.kt
@@ -3,35 +3,14 @@ package app.simplecloud.controller.shared.host
 import io.grpc.ManagedChannel
 import io.grpc.ManagedChannelBuilder
 
-class ServerHost {
+data class ServerHost(
+        val id: String,
+        val host: String,
+        val port: Int
+) {
 
-    private var id: String;
-    private var host: String;
-    private var port: Int;
-    private var endpoint: ManagedChannel
-
-    constructor(id: String, host: String, port: Int) {
-        this.id = id
-        this.host = host
-        this.port = port
-        this.endpoint = createChannel()
-    }
-
-    fun getEndpoint(): ManagedChannel {
-        return endpoint
-    }
-
-    fun getId(): String {
-        return id;
-    }
-
-    fun getHost(): String {
-        return host;
-    }
-
-    fun getPort(): Int {
-        return port
-    }
+    @Transient
+    val endpoint: ManagedChannel = createChannel()
 
     private fun createChannel(): ManagedChannel {
         return ManagedChannelBuilder.forAddress(host, port).usePlaintext().build()
