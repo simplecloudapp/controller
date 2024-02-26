1e60b9c9e8a1fbd9e85a8c63a2de00c6476ebecf
Dayeeet <david@cappell.net>
2024-02-26 21:47:30 +0100

    implement: github packages workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.net.URI
 
 plugins {
     alias(libs.plugins.kotlin)
@@ -19,6 +20,25 @@ allprojects {
 subprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
     apply(plugin = "com.github.johnrengelman.shadow")
+    apply(plugin = "maven-publish")
+
+    configure<PublishingExtension> {
+        repositories {
+            maven {
+                name = "GitHubPackages"
+                url = URI.create("https://maven.pkg.github.com/theSimpleCloud/simplecloud-controller")
+                credentials {
+                    username = System.getenv("USERNAME")
+                    password = System.getenv("TOKEN")
+                }
+            }
+        }
+        publications {
+            register<MavenPublication>("gpr") {
+                from(components["java"])
+            }
+        }
+    }
 
     dependencies {
         testImplementation(rootProject.libs.kotlinTest)
