5f4735d5ea2f0fbd1ec48eb759ae5d18c7bbfac2
dayeeet <david@cappell.net>
2024-11-11 15:10:23 +0100

    refactor: remove unused arguments

--- controller-runtime/src/main/kotlin/app/simplecloud/controller/runtime/launcher/ControllerStartCommand.kt
+++ controller-runtime/src/main/kotlin/app/simplecloud/controller/runtime/launcher/ControllerStartCommand.kt
@@ -45,15 +45,6 @@ class ControllerStartCommand : CliktCommand() {
     val authSecret: String by option(help = "Auth secret", envvar = "AUTH_SECRET_KEY")
         .defaultLazy { AuthFileSecretFactory.loadOrCreate(authSecretPath) }
 
-    private val forwardingSecretPath: Path by option(
-        help = "Path to the forwarding secret (default: .forwarding.secret)",
-        envvar = "FORWARDING_SECRET_PATH"
-    )
-        .path()
-        .default(Path.of(".secrets", "forwarding.secret"))
-
-    val forwardingSecret: String by option(help = "Forwarding secrewt", envvar = "FORWARDING_SECRET")
-        .defaultLazy { AuthFileSecretFactory.loadOrCreate(forwardingSecretPath) }
 
     override fun run() {
         val controllerRuntime = ControllerRuntime(this)
